93b8cdf840427de8de6ed71e3324b7cfa9ddfcce
Murat Ülker <muratulker93@gmail.com>
2022-05-26 21:25:51 +0300

    Header v0.02

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import Navbar from "./Navbar";
-import "../styles/header.css"
+import "../styles/header.css";
 
 const Header = () => {
   return (
     <div className="header">
       <Navbar />
-      <div>Header</div>
+      <div>
+        <h2>Minimize your tabs.</h2>
+        <h2>Find the trends.</h2>
+        <p>
+          Don’t let your computer memories consumes all of those browser tabs.
+          Findtrend let you gathers all of your favorite website into one place.
+        </p>
+        <a href="#!">Get Started 🔥</a>
+      </div>
     </div>
   );
 };
