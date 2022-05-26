e62e8d902728df03d2593032522efcc7bb387c99
Murat Ülker <muratulker93@gmail.com>
2022-05-26 16:21:48 +0300

    Header v0.01

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import "../styles/header.css"
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Navbar />
+      <div>Header</div>
+    </div>
+  );
+};
+
+export default Header;
