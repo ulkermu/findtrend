138ae26603654ae2d4f4b5095de37a1f474cad47
Murat Ülker <muratulker93@gmail.com>
2022-05-30 14:24:48 +0300

    newTab v0.01
    File Creation.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import NewTab from "./components/NewTab";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <NewTab />
     </div>
   );
 }
---
+++ src/components/NewTab.jsx
@@ -0,0 +1,21 @@
+import "../styles/newTab.css";
+
+const NewTab = () => {
+  return (
+    <div className="newTab">
+      <h1>Open new tabs is sh*t</h1>
+      <div className="newTabPanelContainer">
+          <img src="" alt="Solution Right Arrow" />
+          <div className="newTabPanel">
+          <div id="header-socmed-link-1" className="header-socmed-link">
+          <img src="" alt="Company Logo" />
+          <div>Findtrend - Elon Musk</div>
+        </div>
+          </div>
+          <img src="" alt="Needless One Tab Left Arrow" />
+      </div>
+    </div>
+  );
+};
+
+export default NewTab;
