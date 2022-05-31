197d0f60a04e33aba11273714ac291b688c08c4b
Murat Ülker <muratulker93@gmail.com>
2022-05-31 16:20:24 +0300

    NewTab v0.03

--- src/components/NewTab.jsx
+++ src/components/NewTab.jsx
@@ -3,22 +3,26 @@ import rightArrow from "../img/newTab/rightArrow.png";
 import findtrendLogo from "../img/newTab/findTrendLogo.svg";
 import leftArrow from "../img/newTab/leftArrow.png";
 import x from "../img/socmed/x.svg";
+import rightArrowMobil from "../img/newTab/rightArrowMobil.png";
+import leftArrowMobil from "../img/newTab/leftArrowMobil.png";
 
 const NewTab = () => {
   return (
     <div className="newTab">
       <h1>Open new tabs is sh*t</h1>
       <div className="newTab-panel-container">
-        <img src={rightArrow} alt="Solution Right Arrow" />
+        <img className="newTab-panel-container-rightArrow" src={rightArrow} alt="Solution Right Arrow" />
+        <img className="newTab-panel-container-rightArrowMobil" src={rightArrowMobil} alt="Solution Right Arrow" />
         <div className="newTab-panel">
           <div className="newTab-panel-link">
-            <img src={findtrendLogo} alt="Company Logo" />
-            <h4 className="panelTypo">Findtrend - Elon Musk</h4>
+            <img className="newTab-panel-link-logo" src={findtrendLogo} alt="Company Logo" />
+            <h4>Findtrend - Elon Musk</h4>
             <div className="newTab-panel-link-closeBtn-overlay"></div>
             <img className="newTab-panel-link-closeBtn" src={x} alt="Close Button" />
           </div>
         </div>
-        <img src={leftArrow} alt="Needless One Tab Left Arrow" />
+        <img className="newTab-panel-container-leftArrow" src={leftArrow} alt="Needless One Tab Left Arrow" />
+        <img className="newTab-panel-container-leftArrowMobil" src={leftArrowMobil} alt="Needless One Tab Left Arrow" />
       </div>
       <p className="newTab-text">
         A solution for your browser tabs and don’t make your device get slower
