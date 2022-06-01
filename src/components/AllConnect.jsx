e9e7a6ba0d7c44b9823d454e19da458e34570c8f
Murat Ülker <muratulker93@gmail.com>
2022-06-01 16:37:32 +0300

    AllConnect v0.02

--- src/components/AllConnect.jsx
+++ src/components/AllConnect.jsx
@@ -1,15 +1,30 @@
 import socmedLinks from "../socmed.json";
+import "../styles/allConnect.css";
+import tweet1 from "../img/allConnect/tweet1.png";
+import tweet2 from "../img/allConnect/tweet2.png";
+import tweet3 from "../img/allConnect/tweet3.png";
 
 const AllConnect = () => {
   return (
     <div className="allConnect">
       <h2>All platform connect to Findtrend</h2>
-      <div className="allConnect-socmed">
-        {socmedLinks.map((socmed) => (
-          <div key={socmed.id} className="allConnect-socmed-links">
-            <img src={socmed.soc} alt={socmed.socAlt} />
-          </div>
-        ))}
+      <div className="allConnect-socmed-wrapper">
+        <div className="allConnect-socmed">
+          {socmedLinks.map((socmed) => (
+            <div key={socmed.id} className="allConnect-socmed-links">
+              <img src={socmed.soc} alt={socmed.socAlt} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="allConnect-tweets">
+        <img className="allConnect-tweet" src={tweet1} alt="Elon Musk Tweet" />
+        <img
+          className="allConnect-tweet"
+          src={tweet2}
+          alt="Elon Musk Retweet Sawyer Merritt"
+        />
+        <img className="allConnect-tweet" src={tweet3} alt="Elon Musk Tweet" />
       </div>
     </div>
   );
